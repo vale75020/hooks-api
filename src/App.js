ade3fbc1cb4162858231f7cd764765771498255b
vale75020 <valentinosuma@hotmail.com>
2019-09-13 15:31:30 +0200

    fetching data

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import DataFetching from "./components/DataFetching";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetching />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DataFetching.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const DataFetching = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+        console.log(res);
+        setPosts(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []); // [] to fetch only one time
+  return (
+    <div>
+      <div>
+        {posts.map(post => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DataFetching;
